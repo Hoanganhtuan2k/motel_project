a5657b306fe7c5f98842f66e697b4d18e15be0f6
Đào Việt Hoàng <70125833+Hoanganhtuan2k@users.noreply.github.com>
2025-04-22 23:37:49 +0700

    create role api xem

--- src/main/java/com/springmvcapp/controller/ContractController.java
+++ src/main/java/com/springmvcapp/controller/ContractController.java
@@ -25,38 +25,38 @@ public class ContractController {
 
   private final ContractService contractService;
 
-//  @GetMapping("/create/{postId}")
-//  public String createContract(@PathVariable Long postId, Model model) {
-//    PostModelDto post = postService.getPostDetailById(postId);
-//    MotelModel room = contractService.getRoomFromPost(postId);
-//
-//    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//    String currentUsername = authentication.getName();
-//    boolean isAdmin = authentication.getAuthorities().stream()
-//            .anyMatch(auth -> auth.getAuthority().equals("ROLE_ADMIN"));
-//    System.out.println(isAdmin);
-//    ContractModelDto contractDto = new ContractModelDto();
-//    if (room != null) {
-//      contractDto.setRoomId(String.valueOf(room.getId()));
-//      contractDto.setFinalPrice(room.getActualPrice());
-//    }
-//
-//    if (isAdmin) {
-//      List<UserModel> users = contractService.getAllStudents();
-//      model.addAttribute("users", users);
-//    } else {
-//      UserModel currentUser = contractService.getUserByUsername(currentUsername);
-//      contractDto.setStudentId(String.valueOf(currentUser.getId()));
-//      model.addAttribute("currentUser", currentUser);
-//    }
-//
-//    model.addAttribute("contract", contractDto);
-//    model.addAttribute("post", post);
-//    model.addAttribute("room", room);
-//    model.addAttribute("isAdmin", isAdmin);
-//
-//    return "contract_create";
-//  }
+  @GetMapping("/create/{postId}")
+  public String createContract(@PathVariable Long postId, Model model) {
+    PostModelDto post = postService.getPostDetailById(postId);
+    MotelModel room = contractService.getRoomFromPost(postId);
+
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    String currentUsername = authentication.getName();
+    boolean isAdmin = authentication.getAuthorities().stream()
+            .anyMatch(auth -> auth.getAuthority().equals("ROLE_ADMIN"));
+    System.out.println(isAdmin);
+    ContractModelDto contractDto = new ContractModelDto();
+    if (room != null) {
+      contractDto.setRoomId(String.valueOf(room.getId()));
+      contractDto.setFinalPrice(room.getActualPrice());
+    }
+
+    if (isAdmin) {
+      List<UserModel> users = contractService.getAllStudents();
+      model.addAttribute("users", users);
+    } else {
+      UserModel currentUser = contractService.getUserByUsername(currentUsername);
+      contractDto.setStudentId(String.valueOf(currentUser.getId()));
+      model.addAttribute("currentUser", currentUser);
+    }
+
+    model.addAttribute("contract", contractDto);
+    model.addAttribute("post", post);
+    model.addAttribute("room", room);
+    model.addAttribute("isAdmin", isAdmin);
+
+    return "contract_create";
+  }
 
 
 
